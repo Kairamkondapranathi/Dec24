3467fa5907bc5b4efee5c6348978c33824817ec1
Pranathi <89980698+Kairamkondapranathi@users.noreply.github.com>
2022-01-02 16:04:15 +0530

    Add files via upload

---
+++ perfectSquare.js
@@ -0,0 +1,14 @@
+
+const num = parseInt(prompt("Enter a number"))
+const isPerfectSquare = num => {
+   let ind = 1;
+   while(ind * ind <= num){
+      if(ind * ind !== num){
+         ind++;
+         continue;
+      };
+      return true;
+   };
+   return false;
+};
+console.log(isPerfectSquare(num));
